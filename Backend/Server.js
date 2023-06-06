1d3da7548925df652bc218389acea25452228392
phoenix <vgeraud77@gmail.com>
2023-06-06 17:32:23 +0100

    Second commit

---
+++ Backend/Server.js
@@ -0,0 +1,34 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const userRoute = require(./routes/userRoute)
+const app = express()
+
+//Middlewares
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+//Route middleware
+app.use("/api/users", userRoute)
+
+//Routes
+app.get("/", (req, res) => {
+    res.send("Home Page")
+})
+
+// Connect to DB and Start Server
+const PORT = process.env.PORT || 5000;
+mongoose
+     .connect(process.env.MONGO_URI)
+     .then(()=>{
+        
+        app.listen(PORT,() => {
+            console.log(`Server Running on port ${PORT}`);
+        })
+     })
+     .catch((err) => console.log(err))
+        
---
+++ Backend/models/userModel.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose")
+
+const userSchema = moongoose.Schema({
+    name:{
+        type: String,
+        required:[true,"Please add a name"]
+    },
+    email: {
+        type: String,
+        required:[true,"Please add an Email"],
+        unique: true,
+        trim: true,
+        match:[
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,"Please enter a valid email"
+        ]
+    },
+    password: {
+        type: String,
+        required:[true,"Please add a password"],
+        minLength:[6,"Paaword must be equal to 6 characters"],
+        maxlength:[23, "Password less than 23 characters"]
+    },
+    photo:{
+        type: String,
+        required:[true,"Please add a photo"],
+        default: "+234"
+    },
+    bio:{
+        type: String,
+        maxlength:[250,"Bio must not be more than 250"],
+        default:"bio",
+    },
+},
+{
+    timestamps: true, 
+}
+);
+
+const user = mongoose.model("user", userSchema)
+module.exports = User 
